2fc5120fd1ba83886fc5b8ed48b28103da3464cc
sawa-zen <sawadasuiren@gmail.com>
2022-05-29 17:14:38 +0900

    fix

--- co2_checker/co2_checker.py
+++ co2_checker/co2_checker.py
@@ -1,18 +1,41 @@
 import RPi.GPIO as GPIO
 import mh_z19
-import time
 import dht11
 from i2c_lcd import i2c_lcd
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+import time
 
 # initialize GPIO
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 instance = dht11.DHT11(pin=14)
 
-def main():
-  co2 = 0.0
-  temperature = 0.0
-  humidity = 0.0
+co2 = 0.0
+temperature = 0.0
+humidity = 0.0
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    ut = time.time()
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/plain; charset=utf-8')
+    self.end_headers()
+    s = (
+      '# HELP room_co2_concentration_ppm 部屋の二酸化酸素濃度の値です\n'
+      '# TYPE room_co2_concentration_ppm gauge\n'
+      'room_co2_concentration_ppm {}\n'
+      '# HELP room_temperature_degrees_celsius 部屋の温度の値です\n'
+      '# TYPE room_temperature_degrees_celsius gauge\n'
+      'room_temperature_degrees_celsius {}\n'
+      '# HELP room_humidity_percent 部屋の湿度の値です\n'
+      '# TYPE room_humidity_percent gauge\n'
+      'room_humidity_percent {}\n'
+    )
+    self.wfile.write(s.format(co2, temperature, humidity).encode('utf-8'))
+
+def watch_air_environment():
+  global co2, temperature, humidity
   i2c_lcd.init_display()
 
   while True:
@@ -28,6 +51,18 @@ def main():
                                             str(temperature), str(humidity))
     i2c_lcd.send_string_to_display(lcd_message)
     time.sleep(1)
+
+def start_server():
+  host = "localhost"
+  port = 8080
+  server = HTTPServer((host, port), MyHTTPRequestHandler)
+  server.serve_forever()
+
+def main():
+  thread1 = threading.Thread(target=watch_air_environment)
+  thread2 = threading.Thread(target=start_server)
+  thread1.start()
+  thread2.start()
 
 if __name__ == "__main__":
   main()
