782215ba31e7ca3a88d0d889f57039f3694462d1
sawa-zen <sawadasuiren@gmail.com>
2022-05-29 13:51:55 +0900

    fix

--- simple_server.py
+++ simple_server.py
@@ -4,6 +4,7 @@ import time
 
 hoge = 'hoge'
 
+
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
   def do_GET(self):
         self.send_response(200)
@@ -15,17 +16,19 @@ def watch_air_environment():
   while True:
     global hoge
     hoge='huga'
-    print(hoge)
     time.sleep(1)
 
 def start_server():
+  host = "localhost"
+  port = 8080
   server = HTTPServer((host, port), MyHTTPRequestHandler)
   server.serve_forever()
 
-if __name__ == "__main__":
-  host = "localhost"
-  port = 8080
+def main():
   thread1 = threading.Thread(target=watch_air_environment)
   thread2 = threading.Thread(target=start_server)
   thread1.start()
   thread2.start()
+
+if __name__ == "__main__":
+  main()
