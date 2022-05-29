af13ca76fa0f39f49da8d30b7e1d0e516c3d21af
sawa-zen <sawadasuiren@gmail.com>
2022-05-29 11:28:02 +0900

    fix

---
+++ simple_server.py
@@ -0,0 +1,31 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+import time
+
+hoge = 'hoge'
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write('{}'.format(hoge).encode('utf-8'))
+
+def watch_air_environment():
+  while True:
+    global hoge
+    hoge='huga'
+    print(hoge)
+    time.sleep(1)
+
+def start_server():
+  server = HTTPServer((host, port), MyHTTPRequestHandler)
+  server.serve_forever()
+
+if __name__ == "__main__":
+  host = "localhost"
+  port = 8080
+  thread1 = threading.Thread(target=watch_air_environment)
+  thread2 = threading.Thread(target=start_server)
+  thread1.start()
+  thread2.start()
