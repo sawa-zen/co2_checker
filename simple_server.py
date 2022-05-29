10c4eec7e7b4fb30f73853fba71c8b3725c62850
sawa-zen <sawadasuiren@gmail.com>
2022-05-29 14:06:10 +0900

    fix

--- simple_server.py
+++ simple_server.py
@@ -7,10 +7,22 @@ hoge = 'hoge'
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
   def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write('{}'.format(hoge).encode('utf-8'))
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/plain; charset=utf-8')
+    self.end_headers()
+    s = (
+      '# HELP room_co2_concentration_ppm 部屋の二酸化酸素濃度の値です\n'
+      '# TYPE room_co2_concentration_ppm gauge\n'
+      'room_co2_concentration_ppm{room="sawa_zen_studio"} 925.0 1460061337\n'
+      '# HELP room_temperature_degrees_celsius 部屋の温度の値です\n'
+      '# TYPE room_temperature_degrees_celsius gauge\n'
+      'room_temperature_degrees_celsius{room="sawa_zen_studio"} 29.0 1460061337\n'
+      '# HELP room_humidity_percent 部屋の湿度の値です\n'
+      '# TYPE room_humidity_percent gauge\n'
+      'room_humidity_percent{room="sawa_zen_studio"} 31.0 1460061337\n'
+    )
+    self.wfile.write(s.encode('utf-8'))
+    # self.wfile.write('{}'.format(hoge).encode('utf-8'))
 
 def watch_air_environment():
   while True:
